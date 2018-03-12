0f966efa509f3b9f7cc6c97a00dbe2933999a1a3
shimshimkaz <37267539+shimshimkaz@users.noreply.github.com>
2018-03-12 23:18:24 +0900

    red p.5 first red bar

---
+++ TddStudy/TddStudy/Money/Dollar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddStudy.Money
+{
+    public class Dollar
+    {
+        public int amount;
+
+        public Dollar(int amount)
+        {
+        }
+
+        public void times(int multiplier)
+        {
+
+        }
+    }
+}
---
+++ TddStudy/TddStudyTest/MoneyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddStudy.Money;
+
+namespace TddStudyTest
+{
+    [TestClass]
+    public class MoneyTest
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.AreEqual(10, five.amount);
+        }
+    }
+}
