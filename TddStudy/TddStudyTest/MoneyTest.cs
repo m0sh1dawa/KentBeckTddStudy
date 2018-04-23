894bd3f7174026c72ccdfc95a627b5f322eb1a5a
shimshimkaz <37267539+shimshimkaz@users.noreply.github.com>
2018-04-23 21:08:53 +0900

    clean times->Times

--- TddStudy/TddStudyTest/MoneyTest.cs
+++ TddStudy/TddStudyTest/MoneyTest.cs
@@ -11,9 +11,9 @@ namespace TddStudyTest
         public void TestMultiplication()
         {
             Dollar five = new Dollar(5);
-            Dollar product = five.times(2);
+            Dollar product = five.Times(2);
             Assert.AreEqual(10, product.Amount);
-            product = five.times(3);
+            product = five.Times(3);
             Assert.AreEqual(15, product.Amount);
         }
 
