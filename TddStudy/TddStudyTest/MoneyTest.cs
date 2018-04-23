c90f3f9338bea2aa7ba1409c5c3654dbd6e4579d
shimshimkaz <37267539+shimshimkaz@users.noreply.github.com>
2018-04-23 21:08:20 +0900

    clean amout->Amount

--- TddStudy/TddStudyTest/MoneyTest.cs
+++ TddStudy/TddStudyTest/MoneyTest.cs
@@ -12,9 +12,9 @@ namespace TddStudyTest
         {
             Dollar five = new Dollar(5);
             Dollar product = five.times(2);
-            Assert.AreEqual(10, product.amount);
+            Assert.AreEqual(10, product.Amount);
             product = five.times(3);
-            Assert.AreEqual(15, product.amount);
+            Assert.AreEqual(15, product.Amount);
         }
 
         [TestMethod]
