a54858d7441c686384a9f15800a37d13f1e282ef
shimshimkaz <37267539+shimshimkaz@users.noreply.github.com>
2018-05-09 21:10:11 +0900

    green p.26 dollar equality

--- TddStudy/TddStudyTest/MoneyTest.cs
+++ TddStudy/TddStudyTest/MoneyTest.cs
@@ -12,9 +12,9 @@ namespace TddStudyTest
         {
             Dollar five = new Dollar(5);
             Dollar product = five.Times(2);
-            Assert.AreEqual(10, product.Amount);
+            Assert.AreEqual(new Dollar(10), product);
             product = five.Times(3);
-            Assert.AreEqual(15, product.Amount);
+            Assert.AreEqual(new Dollar(15), product);
         }
 
         [TestMethod]
