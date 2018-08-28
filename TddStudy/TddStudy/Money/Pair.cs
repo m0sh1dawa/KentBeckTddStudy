72cb09e0bd43c0c4e9651606f94efec3d228aa62
shimshimkaz <37267539+shimshimkaz@users.noreply.github.com>
2018-08-28 13:46:03 +0900

    green p.106 add Pair class

---
+++ TddStudy/TddStudy/Money/Pair.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddStudy.Money
+{
+    public class Pair
+    {
+        private string from;
+        private string to;
+
+        public Pair(string from, string to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Pair pair = (Pair)obj;
+            return from.Equals(pair.from) && to.Equals(pair.to);
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+}
